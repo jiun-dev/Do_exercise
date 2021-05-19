4222a75b81c173f2c445ac46a4a112987ea4f3ee
jiun-dev <jiun.dev@gmail.com>
2021-05-20 01:07:05 +0900

    Initial Commit

---
+++ DoWorkout/DoWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoWorkoutApp.swift
+//  DoWorkout
+//
+//  Created by shinjiun on 2021/05/20.
+//
+
+import SwiftUI
+
+@main
+struct DoWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
